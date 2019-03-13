1c0b6ee6627b13c7c2731d63f0c4d9bc3c5a72d4
dizzyplay <vovoboss@gmail.com>
2019-03-14 01:01:42 +0900

    addComment api

---
+++ src/api/Comment/addComment/addComment.js
@@ -0,0 +1,26 @@
+import {isAuthenticated} from "../../../middlewares";
+
+export default {
+  Mutation: {
+    addComment: async (_, args, {request, prisma}) => {
+      isAuthenticated(request);
+      const {user} = request;
+      const {text, postId} = args;
+      console.log(text, postId, user);
+      const post = await prisma.createComment({
+        text,
+        post:{
+          connect:{
+            id:postId
+          }
+        },
+        user:{
+          connect:{
+            id:user.id
+          }
+        }
+      });
+      return post
+    }
+  }
+}
