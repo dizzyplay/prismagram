623eb75002c308752d095aa7fb7c16d68c94d6d7
dizzyplay <vovoboss@gmail.com>
2019-03-14 16:00:31 +0900

    add post search api

---
+++ src/api/Post/searchPost/searchPost.js
@@ -0,0 +1,15 @@
+export default {
+  Query: {
+    searchPost: async (_, args, {prisma}) => {
+      const {term} = args;
+      return await prisma.posts({
+        where: {
+          OR: [
+            {location_starts_with: term},
+            {caption_starts_with: term},
+          ]
+        }
+      })
+    }
+  }
+}
