88759095bbc2b1c9c82c2202d2c725afeadb7d8d
dizzyplay <vovoboss@gmail.com>
2019-03-14 23:57:57 +0900

    user profile

--- src/api/User/myProfile/myProfile.js
+++ src/api/User/myProfile/myProfile.js
@@ -5,10 +5,10 @@ export default {
     myProfile: (_, __, { prisma, request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      // const profile = prisma.user({ id: user.id });
-      // const posts = prisma.user({ id: user.id }).posts();
-      return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
-      // return { user: profile, posts };
+      const profile = prisma.user({ id: user.id });
+      const posts = prisma.user({ id: user.id }).posts();
+      // return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+      return { user: profile, posts };
     }
   }
 };
