992b88502102c9277d411357306f41288b484225
dizzyplay <vovoboss@gmail.com>
2019-03-14 23:28:58 +0900

    fragment user

---
+++ src/api/User/myProfile/myProfile.js
@@ -0,0 +1,14 @@
+import { USER_FRAGMENT } from "../../../fragments";
+
+export default {
+  Query: {
+    myProfile: (_, __, { prisma, request, isAuthenticated }) => {
+      isAuthenticated(request);
+      const { user } = request;
+      // const profile = prisma.user({ id: user.id });
+      // const posts = prisma.user({ id: user.id }).posts();
+      return prisma.user({ id: user.id }).$fragment(USER_FRAGMENT);
+      // return { user: profile, posts };
+    }
+  }
+};
