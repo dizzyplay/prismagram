58987fd6d499e7fd6d6c2c036503b0d331cc394f
dizzyplay <vovoboss@gmail.com>
2019-03-14 14:10:10 +0900

    add search user

---
+++ src/api/User/searchUser/searchUser.js
@@ -0,0 +1,16 @@
+export default{
+  Query: {
+    searchUser: async (_,args,{request, prisma})=>{
+      const {term} = args;
+      return await prisma.users({
+        where:{
+          OR:[
+            {username_contains:term},
+            {firstName_contains:term},
+            {lastName_contains:term}
+          ]
+        }
+      });
+    }
+  }
+}
