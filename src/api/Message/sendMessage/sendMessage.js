b684733a9cfa9728da10007aa299985fc17115db
dizzyplay <vovoboss@gmail.com>
2019-03-20 22:23:02 +0900

    chat my way

--- src/api/Message/sendMessage/sendMessage.js
+++ src/api/Message/sendMessage/sendMessage.js
@@ -3,26 +3,36 @@ export default {
     sendMessage: async (_, args, { request, prisma, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const { toUserId, text, roomId } = args;
+      const { toUserId, text } = args;
       const toUser = await prisma.user({ id: toUserId });
-      return await prisma.createMessage({
-        room: {
-          connect: {
-            id: roomId
-          }
-        },
-        text,
-        from: {
-          connect: {
-            username: user.username
-          }
-        },
-        to: {
-          connect: {
-            username: toUser.username
-          }
-        }
-      });
+      if (user.id === toUser.id) {
+        throw Error("You can't send a message yourself.");
+      }
+      const room = await prisma
+        .user({ id: user.id })
+        .rooms({ where: { participants_some: { id: toUser.id } } });
+      if (room.length === 1) {
+        return await prisma.createMessage({
+          room: { connect: { id: room[0].id } },
+          text,
+          to: { connect: { id: toUser.id } },
+          from: { connect: { id: user.id } }
+        });
+      } else if (room.length === 0) {
+        const newRoom = await prisma.createRoom({
+          participants: { connect: [{ id: user.id }, { id: toUser.id }] }
+        });
+        return await prisma.createMessage({
+          room: { connect: { id: newRoom.id } },
+          text,
+          to: { connect: { id: toUser.id } },
+          from: { connect: { id: user.id } }
+        });
+      } else {
+        throw Error(
+          "Chat system got an error. Room value is not correct. check room length"
+        );
+      }
     }
   }
 };
