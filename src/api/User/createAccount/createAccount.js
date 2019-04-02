5ff55f6cebf8024286643e417d9895d890419d52
dizzyplay <vovoboss@gmail.com>
2019-04-03 01:23:55 +0900

    return boolean and cleanup

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,15 +1,19 @@
 export default {
   Mutation: {
-    createAccount: async (_, args, { prisma }) => {
-      const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
-        username,
-        email,
-        firstName,
-        lastName,
-        bio
-      });
-      return user;
-    }
-  }
+    createAccount: async (_, args, {prisma}) => {
+      const {username, email, firstName = '', lastName = '', bio = ''} = args;
+      try {
+        const user = await prisma.createUser({
+          username,
+          email,
+          firstName,
+          lastName,
+          bio,
+        });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 };
