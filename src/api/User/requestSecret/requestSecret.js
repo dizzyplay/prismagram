9005ca3201432887a5ecbaa8a669f724c4c3107f
dizzyplay <vovoboss@gmail.com>
2019-04-03 03:37:38 +0900

    fix too slow response code

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,17 +1,22 @@
-import { generateSecret, sendSecretMail } from "../../../utils";
+import {generateSecret, sendSecretMail} from '../../../utils';
 
 export default {
   Mutation: {
-    requestSecret: async (_, args, { prisma }) => {
-      const { email } = args;
-      const loginSecret = generateSecret();
-      try {
-        await sendSecretMail(email, loginSecret);
-        await prisma.updateUser({ data: { loginSecret }, where: { email } });
-        return true;
-      } catch {
+    requestSecret: async (_, args, {prisma}) => {
+      const {email} = args;
+      const user_exist = await prisma.$exists.user({email});
+      if (user_exist) {
+        const loginSecret = generateSecret();
+        try {
+          await sendSecretMail(email, loginSecret);
+          await prisma.updateUser({data: {loginSecret}, where: {email}});
+          return true;
+        } catch {
+          return false;
+        }
+      } else {
         return false;
       }
-    }
-  }
+    },
+  },
 };
