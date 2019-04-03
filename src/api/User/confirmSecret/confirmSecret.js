0176fc1058b5eb207472b967fe2fd0b7d1bb72d4
dizzyplay <vovoboss@gmail.com>
2019-04-03 18:55:37 +0900

    fix error catch

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -1,23 +1,27 @@
-import { generateToken } from "../../../utils";
+import {generateToken} from '../../../utils';
 
 export default {
   Mutation: {
-    confirmSecret: async (_, args, { prisma }) => {
-      const { email, secret } = args;
-      const user = await prisma.user({ email });
-      if (user.loginSecret === secret) {
-        await prisma.updateUser({
-          where: {
-            id: user.id
-          },
-          data: {
-            loginSecret: ""
-          }
-        });
-        return generateToken(user.id);
-      } else {
-        throw Error("Wrong email/secret ");
+    confirmSecret: async (_, args, {prisma}) => {
+      const {email, secret} = args;
+      const user = await prisma.user({email});
+      try {
+        if (user.loginSecret === secret) {
+          await prisma.updateUser({
+            where: {
+              id: user.id,
+            },
+            data: {
+              loginSecret: '',
+            },
+          });
+          return generateToken(user.id);
+        } else {
+          throw Error('Wrong email/secret ');
+        }
+      } catch {
+        throw Error('비밀키가 존재하지 않습니다 이메일 인증을 다시 해주세요');
       }
-    }
-  }
+    },
+  },
 };
