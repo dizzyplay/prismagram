e752fdde6d79565c882936551fb645e72145ec09
dizzyplay <vovoboss@gmail.com>
2019-04-20 16:55:16 +0900

    add files

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -1,25 +1,28 @@
 export default {
   Post: {
-    isLiked: (parent, __, { request, prisma }) => {
-      const { user } = request;
-      const { id } = parent;
+    files: (parent, __, {request, prisma}) => {
+      return prisma.files({where: {post: {id: parent.id}}});
+    },
+    isLiked: (parent, __, {request, prisma}) => {
+      const {user} = request;
+      const {id} = parent;
       return prisma.$exists.post({
-        AND: [{ id }, { likes_some: { user: { id: user.id } } }]
+        AND: [{id}, {likes_some: {user: {id: user.id}}}],
       });
     },
-    commentsCount: (parent, __, { prisma }) => {
-      const { id } = parent;
+    commentsCount: (parent, __, {prisma}) => {
+      const {id} = parent;
       return prisma
-        .commentsConnection({ where: { post: { id } } })
+        .commentsConnection({where: {post: {id}}})
         .aggregate()
         .count();
     },
-    likesCount: (parent, __, { prisma }) => {
-      const { id } = parent;
+    likesCount: (parent, __, {prisma}) => {
+      const {id} = parent;
       return prisma
-        .likesConnection({ where: { post: { id } } })
+        .likesConnection({where: {post: {id}}})
         .aggregate()
         .count();
-    }
-  }
+    },
+  },
 };
